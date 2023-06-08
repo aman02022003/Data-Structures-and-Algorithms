39dfcf75a34624055ae6858b4aff0dfa4f14af8c
Aman Prajapati <131204371+aman02022003@users.noreply.github.com>
2023-06-08 09:07:55 +0530

    Add files via upload

---
+++ column-sum-print.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+
+void printRowWiseSum(int arr[][3],int row,int col)
+{
+    for(int i=0;i<row;i++)
+    {
+        int sum=0;
+        for(int j=0;j<col;j++)
+        {
+            sum=sum+arr[j][i];
+        }
+
+        cout<<sum<<" ";
+    }
+}
+
+int main()
+{
+    int arr[3][3];
+
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+
+    printRowWiseSum(arr,3,3);
+}
