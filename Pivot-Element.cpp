80e94580a89c35fe5b449d7999dd3eac6c3b6c67
Aman Prajapati <131204371+aman02022003@users.noreply.github.com>
2023-06-22 08:34:33 +0530

    Pivot-Element.cpp

--- Pivot-Element.cpp
+++ Pivot-Element.cpp
@@ -29,45 +29,4 @@ public:
 
         return s;
     }
-    int searchh(vector<int>& nums, int target,int ss,int ee) {
-        int s=ss;
-        int e=ee;
-        while(s<=e)
-        {
-            int mid=s+(e-s)/2;
-            if(nums[mid]==target)
-            {
-                return mid;
-            }
-            if(target>nums[mid])
-            {
-                s=mid+1;
-            }
-            else
-            {
-                e=mid-1;
-            }
-        }
-        return -1;
-    }
-    
-    
-
-    int search(vector<int>& nums, int target) {
-        
-
-        int pivotindex=findpivot(nums);
-        if(target>=nums[0] && target<=nums[pivotindex])
-        {
-            int ans=searchh(nums,target,0,pivotindex);
-            return ans;
-        }
-        if(pivotindex+1<nums.size() && target>=nums[pivotindex+1] && target<=nums[nums.size()-1])
-        {
-            int ans=searchh(nums,target,pivotindex+1, nums.size()-1);
-            return ans;
-        }
-
-        return -1;
-    }
 };
