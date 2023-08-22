7d84fd3701b0cc14869fd265d7d14936c1a9351a
Aman Prajapati <131204371+aman02022003@users.noreply.github.com>
2023-08-22 11:04:07 +0530

    code

---
+++ Intersection-of-Two-Linked-Lists.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* a=headA;
+        ListNode* b=headB;
+        while(a->next && b->next){
+            if(a==b){
+                return a;
+            }
+            a=a->next;
+            b=b->next;
+
+        }
+        if(a->next ==0  && b->next ==0 && a!=b){
+            return 0;
+        }
+        if(a->next ==0){
+            int blen=0;
+            while(b->next){
+                blen++;
+                b=b->next;
+            }
+            while(blen--){
+                headB=headB->next;
+            }
+        }
+        else{
+            int alen=0;
+            while(a->next){
+                alen++;
+                a=a->next;
+            }
+            while(alen--){
+                headA=headA->next;
+            }
+        }
+        while(headA != headB){
+             headA=headA->next;
+             headB=headB->next;
+        }
+        return headA;
+    }
+};
